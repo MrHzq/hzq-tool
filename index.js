8c7ee4cacdd6de37d87eda28e31b7c748ac64fbd
894568474 <894568474@qq.com>
2018-12-12 17:15:02 +0800

    不使用...

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
  * @Author: hzq
  * @Date: 2018-08-28 17:18:05
  * @Last Modified by: hzq
- * @Last Modified time: 2018-12-12 17:10:23
+ * @Last Modified time: 2018-12-12 17:14:28
  * @文件说明: 全局$tool插件
  */
 var validator = require('./validator.js')
 module.exports = {
     install(Vue) {
-        let tool = {
-            ...validator
-        }
+        let tool = Object.assign({}, validator)
         Vue.prototype.$tool = tool
         Vue.$tool = tool
     }
