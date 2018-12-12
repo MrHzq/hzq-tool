be2a889ca34aba4397f8c23355b40991a68efce2
894568474 <894568474@qq.com>
2018-12-12 17:38:32 +0800

    压缩

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
  * @Author: hzq
  * @Date: 2018-08-28 17:18:05
  * @Last Modified by: hzq
- * @Last Modified time: 2018-12-12 17:14:28
+ * @Last Modified time: 2018-12-12 17:38:14
  * @文件说明: 全局$tool插件
  */
-var validator = require('./validator.js')
+var validator = require('./validator.min.js')
 module.exports = {
     install(Vue) {
         let tool = Object.assign({}, validator)
