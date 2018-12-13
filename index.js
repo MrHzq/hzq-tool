49d019f2c00321907f533671c3bee63395a27c03
894568474 <894568474@qq.com>
2018-12-13 10:03:16 +0800

    新增sessionStorage操作

--- index.js
+++ index.js
@@ -2,14 +2,21 @@
  * @Author: hzq
  * @Date: 2018-08-28 17:18:05
  * @Last Modified by: hzq
- * @Last Modified time: 2018-12-12 17:38:14
+ * @Last Modified time: 2018-12-13 10:02:20
  * @文件说明: 全局$tool插件
  */
 var validator = require('./validator.min.js')
 module.exports = {
-    install(Vue) {
+    install(Vue, prefix = 'xkt_') {
         let tool = Object.assign({}, validator)
         Vue.prototype.$tool = tool
         Vue.$tool = tool
+        Vue.prototype.$getItem = key => {
+            return JSON.parse(window.sessionStorage.getItem(prefix + key))
+        }
+        Vue.prototype.$setItem = (key, data) => {
+            window.sessionStorage.setItem(prefix + key, JSON.stringify(data))
+        }
+        Vue.prototype.$copy = obj => JSON.parse(JSON.stringify(obj))
     }
 }
